fc6b399f27a0424de4272a223044fab978defc3e
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-01-15 23:45:05 -0800

    Revert "Keeping one message only"
    This reverts commit 8fa66a868f7ec391eb3eb278d034089fff5aaee9.

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
+Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
+Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
+Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
+Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
+Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
+Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
