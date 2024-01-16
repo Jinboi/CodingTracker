8fa66a868f7ec391eb3eb278d034089fff5aaee9
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-01-15 23:39:37 -0800

    Keeping one message only

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
-Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
-Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
-Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
-Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
-Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
-Console.WriteLine("C# Academy!!!! Hello C# Academy 1 more modification ONE MORE MODIFICATION");
